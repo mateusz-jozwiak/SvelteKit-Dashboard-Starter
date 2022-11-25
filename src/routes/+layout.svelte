d08cce0648715fc6d6ac40a0cc3adcf417fafd9f
Mateusz <mjozwiak@list.pl>
2022-11-25 18:25:11 +0100

    paddsing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,7 +35,7 @@
 		
 
 
-<div class="navbar bg-base-200 mx-auto p-0 sticky top-0 z-30 w-full px-2 py-4 sm:px-4 shadow-xl">
+<div class="navbar bg-base-200 mx-auto p-0 sticky top-0 z-30 w-full px-2 py-2 sm:px-4 shadow-xl">
 	<div class="flex-none">
 		
 		<label for="my-drawer-2" class="btn lg:hidden"><svg class="h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="4" y1="6" x2="20" y2="6" />  <line x1="4" y1="12" x2="20" y2="12" />  <line x1="4" y1="18" x2="20" y2="18" /></svg>
--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -1,6 +1,15 @@
-<script>
-    /** @type {import('./$types').PageData} */
-    export let data;
-</script>
 
-<h1>Profile Settings</h1>
+
+
+<div class="mx-3">  <div class="card lg:card-side bg-base-100 shadow-xl mt-3" >
+
+	<figure><img src="https://placeimg.com/300/300/arch" alt="Album"/></figure>
+	<div class="card-body">
+	  <h2 class="card-title">New album is released!</h2>
+	  <p>Click the button to listen on Spotiwhy app.Click the button to listen on Spotiwhy app.Click thehy app.
+	  </p>
+	  <div class="card-actions justify-end">
+		<button class="btn btn-primary">Listen</button>
+	  </div>
+	</div>
+  </div></div>
